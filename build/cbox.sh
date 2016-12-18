39ad54eef1f18cfe0c0a8fe9c702a6100c9cb9fa
Wei Dong <wdong@wdong.org>
2016-12-18 18:56:15 -0500

    update

--- build/cbox.sh
+++
@@ -1,7 +0,0 @@
-export PATH=/opt/cbox/bin:$PATH
-export CFLAGS=-I/opt/cbox/include
-export CXXFLAGS=-I/opt/cbox/include
-export LDFLAGS="-L/opt/cbox/lib -L/opt/cbox/lib64"
-export CMAKE_INCLUDE_PATH=/opt/cbox/include
-export CMAKE_LIBRARY_PATH=/opt/cbox/include
-export PKG_CONFIG_PATH=/opt/cbox/lib/pkgconfig
